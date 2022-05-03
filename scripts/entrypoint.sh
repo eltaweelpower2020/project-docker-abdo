f48a9dce2ed13f16e681d7153df8765c23b324f7
eltaweelpower2020 <mr.eltaweel@gmail.com>
2022-05-03 14:08:42 +0200

    first commit

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,4 +9,4 @@ python manage.py collectstatic --noinput
 # python manage.py shell -c "from users.models import User; User.objects.create_superuser('admin4', 'admin@example.com', '123qwe456asd')"
 
 
-uwsgi --socket :8000 --master --enable-threads -b 32768 --module libratracking.wsgi 
+uwsgi --socket :8000 --master --enable-threads -b 32768 --module project.wsgi 
